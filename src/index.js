9a7081f6aaf96e12d0ab8227c6c46f3d439d6e1e
Utsav Barnwal <u.barnwal36@gmail.com>
2022-01-22 03:28:03 +0530

    update fauna connection config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const app = require("express")();
 
 const faunadb = require("faunadb");
 
-const client = new faunadb.Client({ secret: process.env.FAUNA_DB_SECRET });
+const client = new faunadb.Client({
+  secret: "fnAEdfUHCUAAQsTE-EgldKuTkUOlNhw9Nk9Qf3jr",
+  domain: "db.us.fauna.com",
+  port: 443,
+});
 
 const {
   Paginate,
@@ -15,7 +19,14 @@ const {
   Lambda,
   Var,
   Join,
+  Ref,
 } = faunadb.query;
+
+app.get("/tweet/:id", async (req, res) => {
+  const doc = await client.query(Get(Ref(Collection("tweets"), req.params.id)));
+
+  res.send(doc);
+});
 
 app.listen(process.env.PORT || 5001, () =>
   console.log("API on http://localhost:5001")
